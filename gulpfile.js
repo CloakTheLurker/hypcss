e648e27a2380cec07bda7ab8aa60fa43bf0c1a1e
Cloak <143768607+CloakTheLurker@users.noreply.github.com>
2024-05-05 23:24:03 -0400

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp')
+const cleanCSS = require('gulp-clean-css')
+const header = require('gulp-header')
+const npm = require('./package.json')
+const desc = `\/*\nhyp ${npm.version}\nCopyright 2016 Krzysztof Kraszewski\nFree to use under MIT licence. \n*\/\n`
+const concat = require('gulp-concat')
+
+gulp.task('default', ['css'], function () {
+  gulp.watch('src/**/*.css', ['css'])
+})
+
+gulp.task('css', function () {
+  return gulp.src(['src/main.css', 'src/typography.css', 'src/grid.css',
+    'src/forms.css', 'src/buttons.css', 'src/utils.css', 'src/code.css'])
+    .pipe(cleanCSS({
+      processImport: false
+    }))
+    .pipe(concat('hyp.min.css'))
+    .pipe(header(desc))
+    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('docs/css'))
+})
